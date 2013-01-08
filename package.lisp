e8a27a4a0461a88abd0f9e0c5b7cbe1f9e5c49e6
Zach Beane <xach@xach.com>
2013-01-07 21:33:37 -0500

    Starting to export the intended interface.

--- package.lisp
+++ package.lisp
@@ -1,7 +1,22 @@
 ;;;; package.lisp
 
 (defpackage #:usenet-legend
-  (:use #:cl))
+  (:use #:cl)
+  ;; General
+  (:export #:id
+           #:storage-pathname
+           )
+  ;; Splitting an archive file
+  (:export #:header-matcher
+           #:split-archive)
+  ;; Corpus-related stuff
+  (:export #:corpus
+           #:load-corpus
+           #:add-article)
+  ;; search
+  (:export #:search-in-corpus
+           #:search-result-page
+           #:article-ids))
 
 (in-package #:usenet-legend)
 
