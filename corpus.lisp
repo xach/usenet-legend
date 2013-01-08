b7e5b1a246f978e2b7d18b5184589e6a23a85684
Zach Beane <xach@xach.com>
2013-01-07 21:33:13 -0500

    Article retrieval: functions for first, last, next, previous articles.

--- corpus.lisp
+++ corpus.lisp
@@ -390,12 +390,42 @@ counts. Signal an error on any inconsistency."
         (file-string (storage-pathname article)))
   article)
 
+(defun valid-id-p (id corpus)
+  (let ((count (article-count corpus)))
+    (<= 0 id (1- count))))
+
+(defun check-article-id (id corpus)
+  (unless (valid-id-p id corpus)
+    (error "Article ID ~D out of range" id)))
+
 (defgeneric load-article-id (id corpus)
   (:method (id corpus)
+    (check-article-id id corpus)
     (restore (make-instance 'corpus-article
                             :id id
                             :corpus corpus)
              t)))
+
+(defgeneric first-article (object)
+  (:method ((corpus corpus))
+    (load-article-id 0 corpus)))
+
+(defgeneric last-article (object)
+  (:method ((corpus corpus))
+    (load-article-id (1- (article-count corpus))
+                     corpus)))
+
+(defgeneric next-article (article)
+  (:method (article)
+    (let ((corpus (corpus article))
+          (next-id (1+ (id article))))
+      (load-article-id next-id corpus))))
+
+(defgeneric previous-article (article)
+  (:method (article)
+    (let ((corpus (corpus article))
+          (previous-id (1- (id article))))
+      (load-article-id previous-id corpus))))
 
 
 (defun term-id-article-bit-vector (term-id corpus)
